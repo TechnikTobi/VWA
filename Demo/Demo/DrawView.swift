2247da1b20d04e0148693f5ec4aa9574b09498d9
Tobias Prisching <tobias.prisching@icloud.com>
2024-07-23 11:01:54 +0200

    Current status

---
+++ Demo/Demo/DrawView.swift
@@ -0,0 +1,139 @@
+//
+//  DrawView.swift
+//  Demo
+//
+//  Created by Tobias Prisching on 23.07.24.
+//
+
+import Foundation
+import UIKit
+
+class DrawView: UIView
+{
+    let linewidth = CGFloat(55);
+    let color     = UIColor.white;
+    
+    var lastPoint: CGPoint!;
+    var lines: [(CGPoint, CGPoint)] = [];    
+    
+    @IBInspectable weak var viewController: ViewController?
+    
+    override func
+    touchesBegan
+    (
+        _ touches: Set<UITouch>, 
+        with event: UIEvent?
+    )
+    {
+        self.lastPoint = touches.first!.location(in: self);
+    }
+    
+    
+    
+    override func
+    touchesMoved
+    (
+        _ touches: Set<UITouch>,
+        with event: UIEvent?
+    )
+    {
+        let nextPoint = touches.first!.location(in: self);
+        
+        lines.append((lastPoint, nextPoint));
+        lastPoint = nextPoint;
+        
+        // Tells the system that the view needs to be (re)drawn
+        setNeedsDisplay();
+    }
+    
+    
+    
+    override func
+    touchesEnded
+    (
+        _ touches: Set<UITouch>,
+        with event: UIEvent?
+    )
+    {
+        self.viewController!.classify();
+    }
+    
+    
+    
+    override func
+    draw
+    (
+        _ rect: CGRect
+    )
+    {
+        super.draw(rect);
+        
+        let drawPath = UIBezierPath();
+        drawPath.lineCapStyle = .round;
+        
+        for line in self.lines
+        {
+            drawPath.move(to: line.0);
+            drawPath.addLine(to: line.1);
+        }
+        
+        // Tell the path how wide to be drawn
+        drawPath.lineWidth = self.linewidth;
+        
+        // Set drawing color
+        color.set();
+        
+        // Perform the function call for actually drawing the path
+        drawPath.stroke();
+    }
+    
+    
+    
+    func
+    getCGcontext
+    ()
+    -> CGContext?
+    {
+        let colorSpace = CGColorSpaceCreateDeviceGray();
+        let bitmapInfo = CGImageAlphaInfo.none.rawValue;
+        
+        let context = CGContext(
+            data: nil,
+            width: 28, height: 28,
+            bitsPerComponent: 8, bytesPerRow: 28,
+            space: colorSpace, bitmapInfo: bitmapInfo
+        );
+        
+        context?.translateBy(x: 0, y: 0);
+        context?.scaleBy(
+            x:  28/self.frame.size.width,
+            y: -28/self.frame.size.height
+        );
+        
+        self.layer.render(in: context!);
+        
+        return context
+    }
+    
+    
+    
+    func
+    getCIimage
+    ()
+    -> CIImage?
+    {
+        let context = self.getCGcontext();
+        let cgImage = context?.makeImage();
+        return CIImage(cgImage: cgImage!);
+    }
+    
+    
+    
+    func
+    clear
+    ()
+    {
+        self.lines = [];
+        setNeedsDisplay();
+    }
+}
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -6,12 +6,75 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // UI variables
+    @IBOutlet weak var uiDrawView:            DrawView!;
+    @IBOutlet weak var uiImageView:           UIImageView!
+    @IBOutlet weak var uiFFNNpredictionLabel: UILabel!
+    @IBOutlet weak var uiCNNpredictionLabel:  UILabel!
+    
+    // Pixelbuffer stuff needed for converting the drawn image
+    // into a value format that the neural network models understand
+    var pixelBuffer: CVPixelBuffer?
+    let pixelBufferAttributes = [
+        kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+        kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
+    ] as CFDictionary;
+    
+    // Loading the neural network models
+    let ffnn = FFNN();
+    let cnn  = CNN();
+    
+    // Initial setup stuff
+    override func
+    viewDidLoad
+    ()
+    {
+        super.viewDidLoad();
+        
+        CVPixelBufferCreate(
+            kCFAllocatorDefault,
+            28,
+            28,
+            kCVPixelFormatType_OneComponent8,
+            pixelBufferAttributes,
+            &pixelBuffer
+        );
+        
+        self.uiDrawView.viewController = self;
+        
+        self.uiFFNNpredictionLabel.isHidden = true;
+        self.uiCNNpredictionLabel.isHidden  = true;
+    }
+    
+    func
+    classify
+    ()
+    {
+        // Get the image from the DrawView
+        let ciImage = self.uiDrawView.getCIimage();
+        
+        // Convert it to a pixelbuffer
+        CIContext().render(ciImage!, to: self.pixelBuffer!);
+        
+        
+        
+        
+    }
+    
+    @IBAction func
+    uiClearButtonPressed
+    (
+        _ sender: Any
+    )
+    {
+        self.uiDrawView.clear();
+        
+        self.uiFFNNpredictionLabel.isHidden = true;
+        self.uiCNNpredictionLabel.isHidden  = true;
     }
 
 
